130071475adac609db1ef23e1ab4c8ada4384295
hwangdaesun <hds650588@gmail.com>
2023-05-23 14:19:46 +0900

    Feat : Add ItemService and TestData File

--- src/main/java/com/tosinsa/toy/TestDataInit.java
+++ src/main/java/com/tosinsa/toy/TestDataInit.java
@@ -1,41 +1,77 @@
 package com.tosinsa.toy;
 
-import com.tosinsa.toy.domain.Address;
-import com.tosinsa.toy.domain.Users;
-import com.tosinsa.toy.domain.UsersRepository;
+import com.tosinsa.toy.domain.*;
+import com.tosinsa.toy.service.ItemService;
+import com.tosinsa.toy.service.MemberService;
+import com.tosinsa.toy.service.OrderService;
+import com.tosinsa.toy.web.ItemForm;
+import com.tosinsa.toy.web.MemberForm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
+
 
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
 
+    private final MemberService memberService;
+    private final ItemService itemService;
+    private final OrderService orderService;
 
-    private final UsersRepository usersRepository;
-
+    /**
+     * 테스트용 데이터 추가
+     */
     @PostConstruct
     public void init() {
 
         System.out.println(" = ");
-
         /**
          * 테스트 유저 추가
          */
 
-        Users users1 = new Users("martin","martin@naver.com", new Address("서울","달빛로","1234"));
-        Users users2 = new Users("dennis","dennis@naver.com", new Address("부산","새벽로","1534"));
-        Users users3 = new Users("sophia","sophia@naver.com", new Address("광주", "도로","0000"));
-        Users users4 = new Users("james","james@naver.com", new Address("대구","가나다로","3333"));
-        Users users5 = new Users("martin","martin@google.com", new Address("서울","가기로","1554"));
+        MemberForm memberForm1 = new MemberForm();
+        memberForm1.setEmail("dure@naver.com");
+        memberForm1.setName("dure");
+        memberForm1.setLoginId("dure");
+        memberForm1.setPassword("1234");
+        memberService.join(memberForm1);
+        Member member1 = memberService.findOne(memberForm1.getLoginId());
 
-        usersRepository.save(users1);
-        usersRepository.save(users2);
-        usersRepository.save(users3);
-        usersRepository.save(users4);
-        usersRepository.save(users5);
+
+        MemberForm memberForm2 = new MemberForm();
+        memberForm2.setEmail("sase@naver.com");
+        memberForm2.setName("sase");
+        memberForm2.setLoginId("sase");
+        memberForm2.setPassword("0000");
+        memberService.join(memberForm2);
+        Member member2 = memberService.findOne(memberForm2.getLoginId());
+
+        /**
+         * 테스트 상품 추가
+         */
+        ItemForm itemForm1 = new ItemForm();
+        itemForm1.setName("itemA");
+        itemForm1.setPrice(10000);
+        itemForm1.setStockQuantity(10);
+
+        ItemForm itemForm2 = new ItemForm();
+        itemForm2.setName("itemB");
+        itemForm2.setPrice(20000);
+        itemForm2.setStockQuantity(20);
+
+        itemService.saveItem(itemForm1);
+        itemService.saveItem(itemForm2);
+
+        /**
+         * 테스트 주문 추가
+         */
+
+        orderService.order(1L,1L,3);
+
+        orderService.order(2L,2L,5);
+
     }
-
 }
