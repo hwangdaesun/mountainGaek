ecd3a0d886d231397d488cf292b3a0d4a67bd914
hwangdaesun <hds650588@gmail.com>
2023-06-21 18:08:38 +0900

    Feat : Add file upload

--- src/main/java/com/tosinsa/toy/service/ItemService.java
+++ src/main/java/com/tosinsa/toy/service/ItemService.java
@@ -1,25 +1,34 @@
 package com.tosinsa.toy.service;
 
-import com.tosinsa.toy.domain.Item;
-import com.tosinsa.toy.domain.ItemRepository;
+import com.tosinsa.toy.domain.file.FileStore;
+import com.tosinsa.toy.domain.file.UploadFile;
+import com.tosinsa.toy.domain.item.Item;
+import com.tosinsa.toy.domain.item.ItemRepository;
 
 import com.tosinsa.toy.web.ItemForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
 
+    private final FileStore fileStore;
     private final ItemRepository itemRepository;
+
     @Transactional
-    public Long saveItem(ItemForm itemForm) {
-        Item item = Item.createItem(itemForm.getName(), itemForm.getPrice(), itemForm.getStockQuantity());
+    public Long saveItem(ItemForm itemForm, MultipartFile multipartFile) throws Exception{
+        UploadFile uploadFile = fileStore.storeFile(multipartFile);
+        Item item = Item.createItem(itemForm.getName(), itemForm.getPrice(), itemForm.getStockQuantity(), uploadFile.getStoreFileName(), uploadFile.getUploadFileName());
         Item saveItem = itemRepository.save(item);
         return saveItem.getId();
     }
@@ -30,6 +39,10 @@ public class ItemService {
 
     public Item findOne(Long itemId) {
         return itemRepository.findById(itemId).orElse(null);
+    }
+
+    public String getStoreFileName(String fileName){
+        return fileStore.getFullPath(fileName);
     }
 
 }
