771bee094fdc10ca24ddd89ef8e96bd8729d87bb
hwangdaesun <hds650588@gmail.com>
2023-06-21 18:04:14 +0900

    Feat : Add Administer SessionConst

--- src/main/java/com/tosinsa/toy/SessionConst.java
+++ src/main/java/com/tosinsa/toy/SessionConst.java
@@ -2,4 +2,5 @@ package com.tosinsa.toy;
 
 public class SessionConst {
     public static final String LOGIN_USER = "loginUser";
+    public static final String LOGIN_ADMINISTER = "loginAdminister";
 }
